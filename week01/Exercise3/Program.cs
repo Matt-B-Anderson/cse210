1e9b8e21315142e8b735eb45f5eee7d7f4e4194f
Matthew Anders <manderson0182@gmail.com>
2025-05-10 16:22:21 -0400

    Completed Exercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        var rng = new Random();
+        int magic = rng.Next(1, 101);
+
+        Console.WriteLine("A random number between 1 and 100 has been chosen.");
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magic)
+        {
+            if (guess < magic)
+                Console.WriteLine("Higher");
+            else
+                Console.WriteLine("Lower");
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
